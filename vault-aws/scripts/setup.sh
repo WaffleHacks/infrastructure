143b1a89d8326151b2b1ee600bfa835215231ff4
Alex Krantz <alex@krantz.dev>
2023-01-08 19:05:33 -0800

    Install AWS SSM agent

--- vault-aws/scripts/setup.sh
+++ vault-aws/scripts/setup.sh
@@ -2,8 +2,17 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Setup NTP
 sudo apt-get update
-sudo apt-get install -y ntp
+sudo apt-get install -y ntp wget
 
 sudo systemctl start ntp
 sudo systemctl enable ntp
+
+# Install AWS SSM Agent
+mkdir /tmp/ssm
+wget -O /tmp/ssm/amazon-ssm-agent.deb https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_arm64/amazon-ssm-agent.deb
+sudo dpkg -i /tmp/ssm/amazon-ssm-agent.deb
+
+sudo systemctl start amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
