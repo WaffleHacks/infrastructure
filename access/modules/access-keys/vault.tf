1dc039b0057d475f0e28139c4650eec3639352b9
Alex Krantz <alex@krantz.dev>
2023-01-16 19:32:54 -0800

    Assign permissions non-dynamically

--- access/modules/access-keys/vault.tf
+++ access/modules/access-keys/vault.tf
@@ -8,21 +8,37 @@ resource "aws_iam_user" "vault" {
   }
 }
 
-resource "aws_iam_user_policy_attachment" "vault" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
-    aws_iam_policy.dynamodb.arn,
-    aws_iam_policy.kms.arn,
-    aws_iam_policy.iam_instance_profile.arn,
-    aws_iam_policy.ssm_parameters.arn,
-  ])
-
-  user       = aws_iam_user.vault.name
-  policy_arn = each.value
-}
-
 resource "aws_iam_access_key" "vault" {
   user   = aws_iam_user.vault.name
   status = "Active"
+}
+
+resource "aws_iam_user_policy_attachment" "vault_ec2" {
+  user       = aws_iam_user.vault.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "vault_vpc" {
+  user       = aws_iam_user.vault.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "vault_dynamodb" {
+  user       = aws_iam_user.vault.name
+  policy_arn = aws_iam_policy.dynamodb.arn
+}
+
+resource "aws_iam_user_policy_attachment" "vault_kms" {
+  user       = aws_iam_user.vault.name
+  policy_arn = aws_iam_policy.kms.arn
+}
+
+resource "aws_iam_user_policy_attachment" "vault_iam_instance_profile" {
+  user       = aws_iam_user.vault.name
+  policy_arn = aws_iam_policy.iam_instance_profile.arn
+}
+
+resource "aws_iam_user_policy_attachment" "vault_ssm_parameters" {
+  user       = aws_iam_user.vault.name
+  policy_arn = aws_iam_policy.ssm_parameters.arn
 }
