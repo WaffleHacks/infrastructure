b7e6dcc5f4cef49abf2d1a64e9b0486c78b74d85
Alex Krantz <alex@krantz.dev>
2023-01-31 15:14:27 -0800

    feat(vault): namespace vault roles under a path

--- vault/deployment/iam.tf
+++ vault/deployment/iam.tf
@@ -69,7 +69,7 @@ data "aws_iam_policy_document" "instance" {
 
     effect    = "Allow"
     actions   = ["sts:AssumeRole"]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/Vault*"]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/vault/*"]
   }
 }
 
