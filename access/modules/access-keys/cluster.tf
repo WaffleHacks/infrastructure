ab550e7c936ef184ad34d2a6bd9fe5a1bd7a7381
Alex Krantz <alex@krantz.dev>
2023-02-14 13:09:09 -0800

    feat(access): generate aws credentials for cluster project

---
+++ access/modules/access-keys/cluster.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_user" "cluster" {
+  name = "cluster"
+  path = "/terraform/"
+
+  tags = {
+    ChildProject = "cluster"
+  }
+}
+
+resource "aws_iam_access_key" "cluster" {
+  user = aws_iam_user.cluster.name
+  status = "Active"
+}
+
+resource "aws_iam_user_policy_attachment" "cluster_iam_role" {
+  user = aws_iam_user.cluster.name
+  policy_arn = aws_iam_policy.iam_role.arn
+}
+
+resource "aws_iam_user_policy_attachment" "cluster_s3" {
+  user = aws_iam_user.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
--- access/modules/access-keys/vault.tf
+++ access/modules/access-keys/vault.tf
@@ -41,27 +41,3 @@ resource "aws_iam_user_policy_attachment" "vault_ssm_parameters" {
   user       = aws_iam_user.vault.name
   policy_arn = aws_iam_policy.ssm_parameters.arn
 }
-
-resource "aws_iam_user" "vault_roles" {
-  name = "vault-roles"
-  path = "/terraform/"
-
-  tags = {
-    ChildProject = "vault"
-  }
-}
-
-resource "aws_iam_access_key" "vault_roles" {
-  user   = aws_iam_user.vault_roles.name
-  status = "Active"
-}
-
-resource "aws_iam_user_policy_attachment" "vault_roles_iam_role" {
-  user       = aws_iam_user.vault_roles.name
-  policy_arn = aws_iam_policy.iam_role.arn
-}
-
-resource "aws_iam_user_policy_attachment" "vault_roles_s3" {
-  user       = aws_iam_user.vault_roles.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
