388755310dedb31d1d4497363d2ae7b5e371990c
Alex Krantz <alex@krantz.dev>
2023-02-14 14:31:18 -0800

    feat(cluster): move created resources into project

--- cluster/modules/agent/outputs.tf
+++ cluster/modules/agent/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   value       = digitalocean_droplet.instance.id
   description = "The ID of the created agent node"
 }
+
+output "urn" {
+  value       = digitalocean_droplet.instance.urn
+  description = "The URN of the created agent node"
+}
--- cluster/modules/storage/outputs.tf
+++ cluster/modules/storage/outputs.tf
@@ -3,6 +3,11 @@ output "id" {
   description = "The ID of the created storage node"
 }
 
+output "urn" {
+  value       = digitalocean_droplet.instance.urn
+  description = "The URN of the created storage node"
+}
+
 output "internal_address" {
   value       = digitalocean_droplet.instance.ipv4_address_private
   description = "The private IP address of the created storage node"
