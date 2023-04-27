e158fea7f0054b940868f61584317faaa7c85f7b
Alex Krantz <alex@krantz.dev>
2023-04-27 09:21:11 -0700

    feat(cluster): remove cluster in favor of fly.io deployment

--- cluster/modules/agent/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = digitalocean_droplet.instance.id
-  description = "The ID of the created agent node"
-}
-
-output "urn" {
-  value       = digitalocean_droplet.instance.urn
-  description = "The URN of the created agent node"
-}
