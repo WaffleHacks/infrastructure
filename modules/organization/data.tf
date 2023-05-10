80593f2c1552d8e543d03444cfb3406f42d8d9f4
Alex Krantz <alex@krantz.dev>
2023-05-09 18:28:05 -0700

    fix(organziations): temporarily disable unused groups data sources

--- modules/organization/data.tf
+++ modules/organization/data.tf
@@ -1,6 +1,6 @@
 data "aws_ssoadmin_instances" "main" {}
 
 locals {
-  instance_arn      = tolist(data.aws_ssoadmin_instances.main.arns)[0]
-  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
+  instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0]
+  # identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
 }
--- modules/organization/groups.tf
+++ modules/organization/groups.tf
@@ -1,43 +1,43 @@
-data "aws_identitystore_group" "operations" {
-  identity_store_id = local.identity_store_id
+# data "aws_identitystore_group" "operations" {
+#   identity_store_id = local.identity_store_id
 
-  alternate_identifier {
-    unique_attribute {
-      attribute_path  = "DisplayName"
-      attribute_value = "Operations"
-    }
-  }
-}
+#   alternate_identifier {
+#     unique_attribute {
+#       attribute_path  = "DisplayName"
+#       attribute_value = "Operations"
+#     }
+#   }
+# }
 
-data "aws_identitystore_group" "people_communications" {
-  identity_store_id = local.identity_store_id
+# data "aws_identitystore_group" "people_communications" {
+#   identity_store_id = local.identity_store_id
 
-  alternate_identifier {
-    unique_attribute {
-      attribute_path  = "DisplayName"
-      attribute_value = "People & Communications"
-    }
-  }
-}
+#   alternate_identifier {
+#     unique_attribute {
+#       attribute_path  = "DisplayName"
+#       attribute_value = "People & Communications"
+#     }
+#   }
+# }
 
-data "aws_identitystore_group" "technology" {
-  identity_store_id = local.identity_store_id
+# data "aws_identitystore_group" "technology" {
+#   identity_store_id = local.identity_store_id
 
-  alternate_identifier {
-    unique_attribute {
-      attribute_path  = "DisplayName"
-      attribute_value = "Technology"
-    }
-  }
-}
+#   alternate_identifier {
+#     unique_attribute {
+#       attribute_path  = "DisplayName"
+#       attribute_value = "Technology"
+#     }
+#   }
+# }
 
-data "aws_identitystore_group" "administrators" {
-  identity_store_id = local.identity_store_id
+# data "aws_identitystore_group" "administrators" {
+#   identity_store_id = local.identity_store_id
 
-  alternate_identifier {
-    unique_attribute {
-      attribute_path  = "DisplayName"
-      attribute_value = "Administrators"
-    }
-  }
-}
+#   alternate_identifier {
+#     unique_attribute {
+#       attribute_path  = "DisplayName"
+#       attribute_value = "Administrators"
+#     }
+#   }
+# }
