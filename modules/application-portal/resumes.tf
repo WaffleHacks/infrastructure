5d3b7129695c50021c7e73091703219fb9323e1b
Alex Krantz <alex@krantz.dev>
2023-05-11 22:34:02 -0700

    feat(application-portal): add cors configuration to resume bucket

--- modules/application-portal/resumes.tf
+++ modules/application-portal/resumes.tf
@@ -17,6 +17,18 @@ resource "aws_s3_bucket_acl" "resumes" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_cors_configuration" "resumes" {
+  bucket = aws_s3_bucket.resumes.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://apply.wafflehacks.org", "https://localhost.localdomain:3000"]
+    expose_headers  = ["x-amz-request-id"]
+    max_age_seconds = 3600
+  }
+}
+
 data "aws_iam_policy_document" "resumes" {
   statement {
     effect = "Allow"
@@ -27,4 +39,12 @@ data "aws_iam_policy_document" "resumes" {
     ]
     resources = ["${aws_s3_bucket.resumes.arn}/*"]
   }
+}
+
+resource "doppler_secret" "api_resume_bucket" {
+  project = "application-portal"
+  config  = "prd"
+
+  name  = "RESUME_BUCKET"
+  value = aws_s3_bucket.resumes.bucket
 }
