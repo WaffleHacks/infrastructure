05931ca19c82347bd7ffca242438be39ea9e36cb
Alex Krantz <alex@krantz.dev>
2023-05-13 20:04:12 -0700

    feat(mailer): complete gcp artifact registry migration

--- modules/mailer/repository.tf
+++ modules/mailer/repository.tf
@@ -1,24 +1,3 @@
-module "image_repository" {
-  source = "../image-repository"
-  providers = {
-    aws = aws.us_east_1
-  }
-
-  name        = "mailer"
-  description = "A generic email interface for all WaffleHacks services."
-
-  github_repository       = "WaffleHacks/mailer"
-  github_actions_provider = var.github_actions_provider_aws
-}
-
-resource "doppler_secret" "github_publish_role" {
-  project = "mailer"
-  config  = "gha"
-
-  name  = "AWS_ROLE"
-  value = module.image_repository.role_arn
-}
-
 module "service_account" {
   source = "../image-registry-service-account"
 
